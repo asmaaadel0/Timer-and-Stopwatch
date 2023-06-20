8a5dfcce33913b738883f4c34a62fe35bf2595c6
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-06-21 02:42:45 +0300

    update timer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 // import TheHeader from './component/layout/TheHeader';
-import StopWatch from './component/StopWatch';
+import StopWatch from "./component/StopWatch";
+import Timer from "./component/Timer";
 
 function App() {
   return (
     <div>
-      {' '}
+      {" "}
       {/* <TheHeader /> */} <StopWatch />
+      <Timer />
     </div>
   );
 }
---
+++ src/component/Timer.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+
+import "./Stopwatch.css";
+import "./Timer.css";
+
+function Timer() {
+  const [time, setTime] = useState("");
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setTime((prevTime) => {
+          if (prevTime === 0) {
+            clearInterval(intervalId);
+            setIsRunning(false);
+          }
+          return prevTime - 1;
+        });
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isRunning]);
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setTime("");
+    setIsRunning(false);
+  };
+
+  const formatTime = (timeInSeconds) => {
+    if (timeInSeconds === "") {
+      return "00:00:00";
+    }
+    const hours = Math.floor(timeInSeconds / 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = timeInSeconds % 60;
+
+    return (
+      String(hours).padStart(2, "0") +
+      ":" +
+      String(minutes).padStart(2, "0") +
+      ":" +
+      String(seconds).padStart(2, "0")
+    );
+  };
+  const handleTime = (event) => {
+    setTime(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1 className="stopwatch"> Timer </h1>{" "}
+      <div className="center">
+        <div className="input">
+          <label> Set Timer in Minutes </label>{" "}
+          <input value={time} onChange={handleTime} type="number" />
+        </div>{" "}
+        <p className="time"> {formatTime(time)} </p>{" "}
+        {!isRunning ? (
+          <button onClick={handleStart}> Start </button>
+        ) : (
+          <button onClick={handleStop}> Stop </button>
+        )}{" "}
+        <button onClick={handleReset}> Reset </button>{" "}
+      </div>{" "}
+    </div>
+  );
+}
+
+export default Timer;
